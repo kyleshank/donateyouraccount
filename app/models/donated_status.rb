92011d29c2bd7d43abe44d12785eecd9c0ae41d1
Kyle Shank <kyle.shank@gmail.com>
2011-02-09 10:02:13 -0500

    retweet retry loop fix

--- app/models/donated_status.rb
+++ app/models/donated_status.rb
@@ -4,12 +4,22 @@ class DonatedStatus < ActiveRecord::Base
   
   validates_presence_of :donation, :status
 
+  validates_uniqueness_of :status_id, :scope => [:donation_id]
+
   after_create :retweet
 
   private
 
   def retweet
-    client = Twitter::Client.new(:oauth_token => self.donation.account.token, :oauth_token_secret => self.donation.account.secret)
-    client.retweet(self.status.twitter_status_id)
+    retries = 0
+    while retries < 5
+      begin
+        client = Twitter::Client.new(:oauth_token => self.donation.account.token, :oauth_token_secret => self.donation.account.secret)
+        client.retweet(self.status.twitter_status_id)
+        break
+      rescue Exception
+        retries += 1
+      end
+    end
   end
 end
