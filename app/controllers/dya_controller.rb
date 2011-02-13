9ad0b1690aa8b672bf89a9af8346cc71fb99fa23
Kyle Shank <kyle.shank@gmail.com>
2011-02-13 10:44:01 -0500

    whoops

--- app/controllers/dya_controller.rb
+++ app/controllers/dya_controller.rb
@@ -1,7 +1,7 @@
 class DyaController < ApplicationController
   def index
     redirect_to dashboard_path and return if current_account
-    @donations = Donation.select("distinct(campaign_id),donations.campaign_id,campaigns.*").joins(:campaign).limit(4)
+    @donations = Donation.select("distinct(campaign_id),donations.campaign_id,campaigns.*").joins(:campaign).order("donations.id DESC").limit(4)
     render :layout => false
   end
 
