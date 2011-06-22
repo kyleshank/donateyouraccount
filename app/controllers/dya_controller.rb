315fc3789b8a44d2efded15fc77b1db312ba8ba4
Kyle Shank <kyle.shank@gmail.com>
2011-06-22 01:06:29 -0400

    campaign nil check

--- app/controllers/dya_controller.rb
+++ app/controllers/dya_controller.rb
@@ -1,7 +1,7 @@
 class DyaController < ApplicationController
   def index
     redirect_to dashboard_path and return if current_account
-    @donations = Donation.select("DISTINCT(campaign_id)").order("id DESC").limit(4)
+    @donations = Donation.select("DISTINCT(campaign_id)").order("id DESC").where("campaign_id IS NOT NULL").limit(4)
     render :layout => false
   end
 
