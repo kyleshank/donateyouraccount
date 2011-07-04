8e0e7a58ee03fd8c72fd51f42c39662fd784f129
Kyle Shank <kyle.shank@gmail.com>
2011-07-04 14:00:13 -0400

    AGPL headers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,21 @@
+##
+# Donate Your Account (donateyouraccount.com)
+# Copyright (C) 2011  Kyle Shank (kyle.shank@gmail.com)
+# http://www.gnu.org/licenses/agpl.html
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+##
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
