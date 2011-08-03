52d14ac7aa0aa1e00af14f3553ad89913a4059ed
Kyle Shank <kyle.shank@gmail.com>
2011-08-03 17:41:50 -0400

    upgraded to jquery 1.6 and new rails-ujs

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,19 +2,6 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 $(function(){
-    /** Buttons **/
-    $(".button").mousedown(function(e){
-        e.preventDefault();
-        $(this).addClass("mousedown");
-    });
-    $(".button").bind("mouseup mouseleave", function(e){
-        e.preventDefault();
-        $(this).removeClass("mousedown");
-    });
-    $(".button").mousemove(function(e){
-        e.preventDefault();
-        return false;
-    });
     $("textarea.embed").click(function(){
         $(this).focus();
         $(this).select();
